00bfcd3adeeb40d7f04f352a03c0f5aed04b50b9
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-04-17 16:52:24 +0300

    17.04 - createPlayers - 1

--- game.js
+++ game.js
@@ -2,9 +2,26 @@ class Game {
     #settings
     #status = 'pending'
 
+    // List of players
+    #player1
+    #player2
+
+    constructor() {
+
+    }
+
+    #createPlayers() {
+        const player1Position = new Position(
+            NumberUtils.getRandomNumber(this.#settings.gridSize.columns),
+            NumberUtils.getRandomNumber(this.#settings.gridSize.rows))
+        this.#player1 = new Player(1, player1Position)
+        const player2Position = new Position(1, 2)
+        this.#player2 = new Player(2, player2Position)
+    }
 
     async start() {
         if (this.#status === 'pending') {
+            this.#createPlayers()
             this.#status = 'in-process'
         }
     }
@@ -21,7 +38,36 @@ class Game {
     get status() {
         return this.#status
     }
+
+    get player1() {
+        return this.#player1
+    }
+
+    get player2() {
+        return this.#player2
+    }
 }
+
+class Position {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+}
+
+class Player {
+    constructor(id, position) {
+        this.id = id
+        this.position = position
+    }
+}
+
+class NumberUtils {
+    static getRandomNumber(max) {
+        return Math.floor(Math.random() * max + 1)
+    }
+}
+
 
 // Import for tests
 module.exports = {
--- game.test.js
+++ game.test.js
@@ -13,12 +13,31 @@ describe('Tests for our cool game', () => {
         expect(game.settings.gridSize.columns).toBe(4)
         expect(game.settings.gridSize.rows).toBe(5)
     })
-    it('start game function test', async () => {
+    it('start game function', async () => {
         const game = new Game()
 
         expect(game.status).toBe('pending')
+        // game start
         await game.start()
 
         expect(game.status).toBe('in-process')
+    })
+    it('check players start position', async () => {
+        const game = new Game()
+
+        game.settings = {
+            gridSize: {
+                columns: 1,
+                rows: 2
+            }
+        }
+        // game start
+        await game.start()
+
+        expect([1]).toContain(game.player1.position.x)
+        expect([1, 2]).toContain(game.player1.position.y)
+
+        expect([1]).toContain(game.player2.position.x)
+        expect([1, 2]).toContain(game.player2.position.y)
     })
 })
