8fc5cc618f6a1ef72f6ac0c9748c2c1e55f7de16
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-04-17 16:23:42 +0300

    17.04 - test - 3

--- game.js
+++ game.js
@@ -1,11 +1,26 @@
- class Game {
+class Game {
     #settings
-     set settings(settings){
-         this.#settings = settings
-     }
-     get settings(){
-         return this.#settings
-     }
+    #status = 'pending'
+
+
+    async start() {
+        if (this.#status === 'pending') {
+            this.#status = 'in-process'
+        }
+    }
+
+
+    set settings(settings) {
+        this.#settings = settings
+    }
+
+    get settings() {
+        return this.#settings
+    }
+
+    get status() {
+        return this.#status
+    }
 }
 
 // Import for tests
--- game.test.js
+++ game.test.js
@@ -4,15 +4,21 @@ describe('Tests for our cool game', () => {
     it('init test', () => {
         const game = new Game()
 
-        game.settings={
+        game.settings = {
             gridSize: {
                 columns: 4,
                 rows: 5
             }
         }
-
         expect(game.settings.gridSize.columns).toBe(4)
         expect(game.settings.gridSize.rows).toBe(5)
     })
+    it('start game function test', async () => {
+        const game = new Game()
 
+        expect(game.status).toBe('pending')
+        await game.start()
+
+        expect(game.status).toBe('in-process')
+    })
 })
